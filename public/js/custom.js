c90857a351f08040a13862c48a9508f06da4de66
Raghav <raghav.agrawal@83incs.com>
2021-03-12 03:16:15 +0530

    ip change to aws ip and port change to 80

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,10 +1,10 @@
-$(function() {
+$(function () {
 
-	"use strict";
+    "use strict";
 
-	var wind = $(window);
+    var wind = $(window);
 
-	var main_height = $(".main-height").outerHeight();
+    var main_height = $(".main-height").outerHeight();
 
     $(".sub-height").outerHeight(main_height);
 
@@ -12,47 +12,47 @@ $(function() {
 
     // scrollIt
     $.scrollIt({
-      upKey: 38,                // key code to navigate to the next section
-      downKey: 40,              // key code to navigate to the previous section
-      easing: 'swing',         // the easing function for animation
-      scrollTime: 600,          // how long (in ms) the animation takes
-      activeClass: 'active',    // class given to the active nav element
-      onPageChange: null,       // function(pageIndex) that is called when page is changed
-      topOffset: -60            // offste (in px) for fixed top navigation
+        upKey: 38,                // key code to navigate to the next section
+        downKey: 40,              // key code to navigate to the previous section
+        easing: 'swing',         // the easing function for animation
+        scrollTime: 600,          // how long (in ms) the animation takes
+        activeClass: 'active',    // class given to the active nav element
+        onPageChange: null,       // function(pageIndex) that is called when page is changed
+        topOffset: -60            // offste (in px) for fixed top navigation
     });
 
     // navbar scrolling background
-    wind.on("scroll",function () {
+    wind.on("scroll", function () {
 
         var bodyScroll = wind.scrollTop(),
             navbar = $(".navbar-default"),
             logo = $(".navbar .logo> img");
 
-        if(bodyScroll > 300){
+        if (bodyScroll > 300) {
 
             navbar.addClass("nav-scroll");
-            logo.attr('src', 'http://localhost:3001/images/sochGloballogo5.png');
+            logo.attr('src', 'http://15.206.74.51:3001/images/sochGloballogo5.png');
 
-        }else{
+        } else {
 
             navbar.removeClass("nav-scroll");
-            logo.attr('src', 'http://localhost:3001/images/sochGloballogo5.png');
+            logo.attr('src', 'http://15.206.74.51:3001/images/sochGloballogo5.png');
         }
     });
 
 
 
     // button scroll to top
-    wind.on("scroll",function () {
+    wind.on("scroll", function () {
 
         var bodyScroll = wind.scrollTop(),
             button_top = $(".button-top");
 
-        if(bodyScroll > 700){
+        if (bodyScroll > 700) {
 
             button_top.addClass("button-show");
 
-        }else{
+        } else {
 
             button_top.removeClass("button-show");
         }
@@ -60,75 +60,75 @@ $(function() {
 
 
 
-	// owlCarousel
-	$('.slider .owl-carousel').owlCarousel({
-		items: 1,
-		loop:true,
-        mouseDrag:false,
-        autoplay:true,
-        smartSpeed:500,
+    // owlCarousel
+    $('.slider .owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        mouseDrag: false,
+        autoplay: true,
+        smartSpeed: 500,
         dots: true,
         nav: true,
         navText: ['<span>Prev</span>',
-        '<span>Next</span>'],
+            '<span>Next</span>'],
         animateOut: 'fadeOut'
-	});
+    });
 
     // owlCarousel
     $('.carousel .owl-carousel').owlCarousel({
         items: 1,
-        loop:true,
-        mouseDrag:false,
-        autoplay:true,
-        smartSpeed:500,
+        loop: true,
+        mouseDrag: false,
+        autoplay: true,
+        smartSpeed: 500,
         dots: true,
         nav: true,
         navText: ['<span>Prev</span>',
-        '<span>Next</span>']
+            '<span>Next</span>']
     });
 
 
     // owlCarousel
     $('.team .owl-carousel').owlCarousel({
         items: 1,
-        loop:true,
-        mouseDrag:false,
-        autoplay:true,
-        smartSpeed:500
+        loop: true,
+        mouseDrag: false,
+        autoplay: true,
+        smartSpeed: 500
     });
 
     // owlCarousel
     $('.clients .owl-carousel').owlCarousel({
-        items:1,
-        loop:true,
+        items: 1,
+        loop: true,
         margin: 15,
-        mouseDrag:false,
-        autoplay:true,
-        smartSpeed:500
+        mouseDrag: false,
+        autoplay: true,
+        smartSpeed: 500
     });
 
     // owlCarousel
     $('.posts .owl-carousel').owlCarousel({
-        items:3,
-        loop:true,
+        items: 3,
+        loop: true,
         margin: 30,
         dots: false,
         nav: true,
         navText: ['<span class="ion-chevron-left"></span>',
-        '<span class="ion-chevron-right"></span>'],
-        mouseDrag:false,
-        autoplay:true,
-        smartSpeed:500,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1
+            '<span class="ion-chevron-right"></span>'],
+        mouseDrag: false,
+        autoplay: true,
+        smartSpeed: 500,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:2
+            600: {
+                items: 2
             },
-            1000:{
-                items:3
+            1000: {
+                items: 3
             }
         }
     });
@@ -141,22 +141,22 @@ $(function() {
     // progress bar
     wind.on('scroll', function () {
         $(".skills-progress span").each(function () {
-            var bottom_of_object = 
-            $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = 
-            $(window).scrollTop() + $(window).height();
+            var bottom_of_object =
+                $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window =
+                $(window).scrollTop() + $(window).height();
             var myVal = $(this).attr('data-value');
-            if(bottom_of_window > bottom_of_object) {
+            if (bottom_of_window > bottom_of_object) {
                 $(this).css({
-                  width : myVal
+                    width: myVal
                 });
             }
         });
     });
 
 
-	// Tabs
-    $(".tabs").on("click", "li", function(){
+    // Tabs
+    $(".tabs").on("click", "li", function () {
 
         var myID = $(this).attr("id");
 
@@ -182,16 +182,16 @@ $(function() {
 
     // isotope
     $('.gallery').isotope({
-      // options
-      itemSelector: '.items'
+        // options
+        itemSelector: '.items'
     });
 
     var $gallery = $('.gallery').isotope({
-      // options
+        // options
     });
 
     // filter items on button click
-    $('.filtering').on( 'click', 'span', function() {
+    $('.filtering').on('click', 'span', function () {
 
         var filterValue = $(this).attr('data-filter');
 
@@ -199,7 +199,7 @@ $(function() {
 
     });
 
-    $('.filtering').on( 'click', 'span', function() {
+    $('.filtering').on('click', 'span', function () {
 
         $(this).addClass('active').siblings().removeClass('active');
 
@@ -208,13 +208,13 @@ $(function() {
 
     // magnificPopup
     $('.portfolio .link').magnificPopup({
-      delegate: 'a',
-      type: 'image'
+        delegate: 'a',
+        type: 'image'
     });
 
 
     // accordion
-    $(".accordion").on("click",".title", function () {
+    $(".accordion").on("click", ".title", function () {
 
         $(this).next().slideDown();
 
@@ -230,7 +230,7 @@ $(function() {
         direction: 'left-right',
         opacity: [1, 1],
         isPausedWhenNotInView: true,
-        states : {
+        states: {
             "default-state": {
                 gradients: [
                     ['#AA076B', '#61045F'],
@@ -243,7 +243,7 @@ $(function() {
 
 });
 
-$(window).on("load",function (){
+$(window).on("load", function () {
 
     // Preloader
     $(".loading").fadeOut(500);
@@ -260,8 +260,7 @@ $(window).on("load",function (){
                 type: "POST",
                 url: url,
                 data: $(this).serialize(),
-                success: function (data)
-                {
+                success: function (data) {
                     var messageAlert = 'alert-' + data.type;
                     var messageText = data.message;
 
@@ -278,6 +277,6 @@ $(window).on("load",function (){
 
 });
 
-jQuery(function(){
-      jQuery("#bgndVideo").YTPlayer();
-    });
+jQuery(function () {
+    jQuery("#bgndVideo").YTPlayer();
+});
